c28b7e507a3eb33a57ae439de37d500f835f1247
marty-suzuki <s1180183@gmail.com>
2019-02-16 12:42:54 +0900

    fix directory structures

---
+++ NoticeObserveKit/NoticeObserveKit.h
@@ -0,0 +1,19 @@
+//
+//  NoticeObserveKit.h
+//  NoticeObserveKit
+//
+//  Created by marty-suzuki on 2019/02/16.
+//  Copyright © 2019 marty-suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NoticeObserveKit.
+FOUNDATION_EXPORT double NoticeObserveKitVersionNumber;
+
+//! Project version string for NoticeObserveKit.
+FOUNDATION_EXPORT const unsigned char NoticeObserveKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoticeObserveKit/PublicHeader.h>
+
+
