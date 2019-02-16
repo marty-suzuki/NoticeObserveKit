7833f30a6095bce418d8faefe97041dcd1621316
marty-suzuki <s1180183@gmail.com>
2019-02-16 17:01:50 +0900

    extra sample files

--- Example/Pods/Target Support Files/NoticeObserveKit/NoticeObserveKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NoticeObserveKit : NSObject
-@end
-@implementation PodsDummy_NoticeObserveKit
-@end
--- Example/Pods/Target Support Files/Pods-NoticeObserveKitSample/Pods-NoticeObserveKitSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NoticeObserveKitSample : NSObject
-@end
-@implementation PodsDummy_Pods_NoticeObserveKitSample
-@end
